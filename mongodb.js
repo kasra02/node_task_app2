119cfd4700caf36a2d8553132beb57c1c63bdbe7
kasra <kasra.niroumand@gmail.com>
2020-12-26 00:38:18 +0330

    task collection is added

--- mongodb.js
+++ mongodb.js
@@ -12,8 +12,50 @@ MongoClient.connect(connectionURLL,{useNewUrlParser:true}, (err,client)=>{
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name:'kasra',
-        age:23
+    // db.collection('users').insertOne({
+    //     name:'kasra',
+    //     age:23
+    // },(err, result)=>{
+    //     if(err){
+    //         return console.log('unable to insert')
+    //     }
+    //     console.log(result.ops)
+    // })
+    // db.collection('users').insertMany([
+    //     {
+    //         name:'are',
+    //         age:29
+    //     },
+    //     {
+    //         name:'tara',
+    //         age:42
+    //     }
+    // ],(err,result)=>{
+    //    if(err){
+    //        return console.log('unablle to insert')
+    //    }
+    //    console.log(result.ops)
+    // })
+    db.collection('tasks').insertMany([
+        {
+            task:"code until death",
+            complete:false
+        },
+        {
+            task:"drink until death",
+            complete:false
+        },
+        {
+            task:"be rich like musk till death",
+            complete:true
+        }
+    ],(err, result)=>{
+        if(err){
+            return console.log('unablle to insert')
+        }
+        console.log(result.ops)
     })
+
+
+
 })
